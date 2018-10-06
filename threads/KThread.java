7a1675b85a041b004bec27574ca5c358a5e0a089
Vishnu Mullick <vm46108n@pace.edu>
2018-10-06 10:56:58 -0700

    Second commit

--- threads/KThread.java
+++ threads/KThread.java
@@ -388,7 +388,7 @@ public class KThread {
 	
 	public void run() {
 	    for (int i=0; i<5; i++) {
-		System.out.println("*** thread " + which + " looped "
+		System.out.println("*** Hello, sweet thread " + which + " looped "
 				   + i + " times");
 		currentThread.yield();
 	    }
