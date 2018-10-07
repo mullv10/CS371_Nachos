0d77411defd355e11faa6a889b2e0634784fb23c
Vishnu Mullick <vm46108n@pace.edu>
2018-10-07 13:11:36 -0700

    Final assignment 1 commit

--- threads/HashTable.java
+++ threads/HashTable.java
@@ -133,8 +133,9 @@ public class HashTable {
     
     /*find an element from the hashtable based on key*/ 
     public int get(String key) throws Exception{
-    	int hash = Hash(key, debug_flag) / SIZE;
+    	int hash = Hash(key, debug_flag) % SIZE;
     	if(hTable[hash].getKey() == null){
+    		System.err.println(hTable[hash].getKey());
     		throw new HashTableException("Does not exist");
     	} else if(hTable[hash].getKey() == key){
     		return hTable[hash].getValue();
@@ -153,16 +154,21 @@ public class HashTable {
     /*delete an element from the hashtable based on key*/
     public void out(String key) throws Exception{
     	int hash = Hash(key, debug_flag) % SIZE;
-    	if(hTable[hash].getKey() == null){
+		//(debug)System.out.println("Key is..." + hTable[hash].getKey() + "," + key);
+		//(debug)System.out.println("next" + hTable[hash].getNext());
+    	if(hTable[hash].getKey() == null) {
     		throw new HashTableException("Does not exist");
     	} else if(hTable[hash].getKey() == key){
     		if(hTable[hash].getNext() != null){
     			hTable[hash] = hTable[hash].getNext();
     		} else {
         		hTable[hash].setKey(null);
+        		count --;
     		}
     	} else {
+  
     		KVPair temp = hTable[hash];
+      		System.out.println("iin KVPair..." + temp);
     		while(temp.getNext() != null) {
         		KVPair pred = temp;
     			temp = temp.getNext();
@@ -182,22 +188,18 @@ public class HashTable {
     	System.out.println("HashTable seftTest begins...");
     	try {
     		HashTable table = new HashTable();
-    		System.out.println("After HashTable creation...");
     		table.put("abc", 30);
     		table.put("bcd", 32);
     		table.put("cde", 34);
-    		System.out.println("After all puts...");
     		Lib.assertTrue(table.get("abc") == 30);
     		Lib.assertTrue(table.get("bcd") == 32);
     		Lib.assertTrue(table.get("cde") == 34);
-    		System.out.println("After gets...");
     		Lib.assertTrue(table.getCount() == 3);
-    		
     		table.out("bcd");
     		Lib.assertTrue(table.getCount() == 2);
-
+    		
     		//turn on debug flag, use the simpleHash which tends to clash easily
-    		HashTable table2 = new HashTable(true);
+    		HashTable table2 = new HashTable();
     		table2.put("abc", 30);
     		table2.put("bca", 32);
     		table2.put("cba", 34);
